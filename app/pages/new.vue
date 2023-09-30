adb193cef547204c959a9fa884a6fcf9ae2f91b4
InfiniteTwilight <66224538+InfiniteTwilight@users.noreply.github.com>
2023-09-30 17:46:06 +0300

    Update new.vue

--- app/pages/new.vue
+++ app/pages/new.vue
@@ -3,4 +3,15 @@
     <!-- This page correctly has only one single root element -->
     Page content
   </div>
+
+  <!-- Yandex.RTB R-A-3154841-1 -->
+<div id="yandex_rtb_R-A-3154841-1"></div>
+<script>
+window.yaContextCb.push(()=>{
+	Ya.Context.AdvManager.render({
+		"blockId": "R-A-3154841-1",
+		"renderTo": "yandex_rtb_R-A-3154841-1"
+	})
+})
+</script>
 </template>
