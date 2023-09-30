752fd8f106f5a79cc2485f3e2636f4a99e353171
InfiniteTwilight <66224538+InfiniteTwilight@users.noreply.github.com>
2023-09-30 17:50:16 +0300

    Update new.vue

--- app/pages/new.vue
+++ app/pages/new.vue
@@ -4,6 +4,11 @@
     Page content
   </div>
 
+<!-- Yandex.RTB -->
+<script>window.yaContextCb=window.yaContextCb||[]</script>
+<script src="https://yandex.ru/ads/system/context.js" async></script>
+
+  
   <!-- Yandex.RTB R-A-3154841-1 -->
 <div id="yandex_rtb_R-A-3154841-1"></div>
 <script>
@@ -14,4 +19,17 @@ window.yaContextCb.push(()=>{
 	})
 })
 </script>
+
+<!-- Yandex.RTB R-A-3154841-2 -->
+<div id="yandex_rtb_R-A-3154841-2"></div>
+<script>
+window.yaContextCb.push(()=>{
+	Ya.Context.AdvManager.render({
+		"blockId": "R-A-3154841-2",
+		"renderTo": "yandex_rtb_R-A-3154841-2",
+		"type": "feed"
+	})
+})
+</script>
+  
 </template>
